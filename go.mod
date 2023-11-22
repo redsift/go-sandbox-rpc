1828ce9e065d8dbd3d5169299bf719a60deb197a
Julian Picht <julian.picht@redsift.io>
2023-11-22 14:05:34 +0100

    make v1 backwards compatible

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/redsift/go-sandbox-rpc/v3
 
 go 1.21
+
+require github.com/redsift/go-sandbox-rpc v0.3.0
--- v1.go
+++ v1.go
@@ -4,46 +4,14 @@
 // Copyright (c) 2017 RedSift Limited. All rights reserved.
 package sandboxrpc
 
-// ComputeRequestV1 contains the parameters to invoke the node implementation function.
-type ComputeRequestV1 struct {
-	In    *StoredDataQuantum   `json:"in"`
-	Query []string             `json:"query,omitempty"`
-	With  *StoredDataQuantum   `json:"with,omitempty"`
-	Get   []*StoredDataQuantum `json:"get,omitempty"`
-}
+import legacy "github.com/redsift/go-sandbox-rpc"
 
-// ResponseV1 is returned by the sandbox.
-type ResponseV1 struct {
-	Out   []*ComputeResponseV1 `json:"out,omitempty"`
-	Error interface{}          `json:"error,omitempty"`
-	Stats interface{}          `json:"stats,omitempty"` // timings and resource usage to be booked against this sift TODO: nail down
-}
-
-// A single unit of operation
-type StoredDataQuantum struct {
-	Bucket string        `json:"bucket"`
-	Key    string        `json:"key,omitempty"`
-	Data   []*StoredData `json:"data"`
-}
-
-type Data struct {
-	Key   string `json:"key"`
-	Value []byte `json:"value"`
-	Epoch int64  `json:"epoch"`
-}
-
-type StoredData struct {
-	Data
-	Generation   uint32 `json:"generation"`
-	TTL          uint32 `json:"ttl"`
-	DiscardValue bool   `json:"-"`
-	Gather       bool   `json:"-"`
-}
-
-type ComputeResponseV1 struct {
-	StoredData
-	Name string `json:"name"`
-}
+type ComputeRequestV1 = legacy.ComputeRequest
+type ResponseV1 = legacy.Response
+type StoredDataQuantum = legacy.StoredDataQuantum
+type Data = legacy.Data
+type StoredData = legacy.StoredData
+type ComputeResponseV1 = legacy.ComputeResponse
 
 func NewComputeResponse(name string, key string, value []byte, epoch int64, generation uint32) ComputeResponseV1 {
 	return ComputeResponseV1{
